59bbacee1581e4fdd96f954bce979abac600baaa
John Zhao <john@zondr.com>
2015-11-30 00:59:31 -0600

    task id controller

--- app/controllers/api/v1/ocr_controller.rb
+++ app/controllers/api/v1/ocr_controller.rb
@@ -2,8 +2,14 @@ require 'parse'
 
 class Api::V1::OcrController < ApiController
 	def post
-		parse_url = params[:parse_url]
-		@courses = parse(parse_url)
+		task_id = params[:task_id]
+		response = HTTParty.get('https://cloud.ocrsdk.com/listFinishedTasks',
+			:headers => { "Authorization" => "Basic S2VlcCBVcCBBcHA6R293MVVLcW1XL0FNSXRkZ0Q0SWhHMTVJIA=="})
+		
+		# @courses = parse(parse_url)
+		p response.find_by[id: task_id]
+		render json: '{yes:no}'
+
 	end
 
 	def index
