183fdcb84708821860013f0010e2483bf8152cdc
John Zhao <john@zondr.com>
2015-12-14 18:28:43 -0600

    search uuid

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApiController
 	def create
 		device_id = params[:uuid]
+		p device_id
 
 		#check if user's device exist
 		@user = User.find_by(device_id: device_id)
@@ -11,6 +12,7 @@ class Api::V1::UsersController < ApiController
 		else
 			# if user doesn't exist, create new user and return token
 			@user = User.new
+			@user.device_id = device_id
 			@user.save
 
 		end
