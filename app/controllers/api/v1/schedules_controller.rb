fc0bd901f88eb263d56581f34f72fa9c0f7e3906
John Zhao <john@zondr.com>
2015-12-17 19:55:48 -0600

    change schedule to token

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SchedulesController < ApplicationController
 
 	def index
-		current_user = User.find_by_id(params[:user_id])
+		current_user = User.find_by_token(params[:token])
 		day = params[:day].to_s
 		@schedule = current_user.schedule_for(day)
 	end
